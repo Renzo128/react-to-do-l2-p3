832f709a8ea5a67a11383ab8e763db3edf7191e3
Renzo <renzovanijzendoorn2@gmail.com>
2022-04-08 13:54:20 +0200

    getting image

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,61 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
+import { Routes, Route, Link } from "react-router-dom";
 const App = () => {
 
 
   const [pokemon, setPokemon] = useState([]);
-//      .catch((error) => {
-//  console.log(error);
-//});
-  // https://pokeapi.co/api/v2/pokemon/5
+  const [pokemonImage, setPokemonImage] = useState([]);
 
-  useEffect(() => {
-    const fetchpokemon = fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => json)
+  useEffect(async () => {
+    try{ 
+      let response = await axios(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`)
+      let data = await response.data.results
+      setPokemon(data);
+      const pokeName = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+      for (let i = 0; i < data.length; i++) {
+        const poke = pokeName.data.results[i];
+        const pokeImage = await axios.get(poke.url);
+        const pokeObj = {
+          name: poke.name,
+          image: pokeImage.data.sprites.front_default
+        }
+      console.log(pokeObj);
 
-    setPokemon(fetchpokemon);
-  }, []);
+        setPokemonImage(pokeObj);
+      }
+    } catch(error) {
+       console.error(error.message);
+    }
+  },[]);
+
+  // useEffect(async() => {
+  //   let response = await axios("https://pokeapi.co/api/v2/pokemon/1");
+  //   let data = await response.data;
+  //   console.log(response);
+  //   setPokemonImage(data);
+  // })
+
+
+
   return (
 
-    <div>
-
+    <div >
+      {
+        console.log(pokemon)
+        
+      }
        {pokemon.map((pkmn) => { 
-        return <h5>{pkmn.title}</h5>;
+        return <h5 key={pkmn.url} id={pkmn.url}>{pkmn.name}</h5>;
+
       })}  
+      <div>
+        {
+          <img src={pokemonImage.image} ></img>
+        }
+      </div>
+
     </div>
   );
 };
